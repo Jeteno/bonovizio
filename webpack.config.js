e9fafc1f47041cda1e430f61c840aa615cc2ea70
Jeteno <macsim.sytov@yandex.ru>
2023-08-17 17:00:01 +0300

    bonovizio

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       path: path.resolve(__dirname, 'dist'),
       filename: 'main.js'
    },
-   mode: 'production',
+   mode: 'development',
    plugins: [
       new MiniCssExtractPlugin(),
       new HtmlWebpackPlugin({
